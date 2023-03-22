2c27debc0db957436e5216d1b92cb2302cf8b1ee
Parul Agarwal <polk54@gmail.com>
2023-03-22 13:29:21 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 // Load the data
 d3.csv("https://datawrapper.dwcdn.net/AS6w5/6/data.csv", function(data) {
 
-  // Set the dimensions and margins of the chart
-  var margin = {top: 20, right: 20, bottom: 30, left: 40},
-      width = 600 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
+  // set the dimensions and margins of the graph
+var margin = {top: 20, right: 30, bottom: 40, left: 90},
+    width = 600 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-  // Append the SVG object to the chart div and set the dimensions
-  var svg = d3.select("#chart")
-    .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-      .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
+// append the svg object to the body of the page
+var svg = d3.select("#chart")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+//Read the data
+d3.csv("data.csv", function(data) {
 
   // Add X axis
   var x = d3.scaleLinear()
-    .domain([0, 80000])
+    .domain([-50, 50])
     .range([ 0, width ]);
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
@@ -25,7 +28,7 @@ d3.csv("https://datawrapper.dwcdn.net/AS6w5/6/data.csv", function(data) {
 
   // Add Y axis
   var y = d3.scaleLinear()
-    .domain([0, 140])
+    .domain([-50, 50])
     .range([ height, 0]);
   svg.append("g")
     .call(d3.axisLeft(y));
@@ -36,24 +39,9 @@ d3.csv("https://datawrapper.dwcdn.net/AS6w5/6/data.csv", function(data) {
     .data(data)
     .enter()
     .append("circle")
-      .attr("cx", function (d) { return x(d.gdp_per_capita); })
-      .attr("cy", function (d) { return y(d.life_expectancy); })
+      .attr("cx", function (d) { return x(d.change_2020); } )
+      .attr("cy", function (d) { return y(d.change_2021); } )
       .attr("r", 5)
-      .style("fill", "#69b3a2");
-
-  // Add X axis label
-  svg.append("text")
-    .attr("text-anchor", "end")
-    .attr("x", width)
-    .attr("y", height + margin.top + 20)
-    .text("GDP per capita (USD)");
-
-  // Add Y axis label
-  svg.append("text")
-    .attr("text-anchor", "end")
-    .attr("transform", "rotate(-90)")
-    .attr("y", -margin.left + 20)
-    .attr("x", 0)
-    .text("Life expectancy (years)");
+      .style("fill", "#69b3a2")
 
 });
